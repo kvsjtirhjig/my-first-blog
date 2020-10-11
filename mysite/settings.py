f96a2af56b2b7a93664ad883822e9f1b5cdff91b
asdjfkaj4tjfds <a.ki.123436@gmail.com>
2020-10-11 12:05:32 +0900

    Gitguardian Modified....

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b-2%e+b7oro2!r3+l4u*_+oi91n18+s8zdi5unhqzi*c$_rcsw'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
